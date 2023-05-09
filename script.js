dc13a520c3a3dacbd25b7e750886d8be44a56f28
Justin Delgado <delgadojustin100@yahoo.com>
2023-05-08 21:03:56 -0400

    finished my first ever js project

--- script.js
+++ script.js
@@ -2,27 +2,50 @@ let wins =  0;
 let losses = 0;
 let draws = 0;
 
-let choices = ["rock", "paper", "scissor"];
+start = document.querySelector("#start");
 
+let choices = ["r", "p", "s"];
 
-
-
+play = () => {
+    start = document.querySelector("#start");
+    start.addEventListener("click", startGame);
+}
 
 startGame = () => {
 
-    function computerPlay() {
-        return choices[Math.floor(Math.random() * choices.length)];
-    }
-
-    let playerSelection = prompt("Rock, Paper or Scissor?");
-    let computerSelection = computerPlay();
-    playRound(playerSelection, computerSelection);
-
-   
-    if (!userChoice) {
+    let playerSelection = prompt("r, p or s?");
+    if (playerSelection === null) {
         return;
-      }
+    }
+        
+
+     computerSelection = choices[Math.floor(Math.random() * choices.length)];
+    
+
+      alert(`computer chose ` + computerSelection);
+
+    if (playerSelection === computerSelection) {
+        draws++;
+        alert("It's a draw!");
+    }
+     
+    else if (
+        (playerSelection === "r" && computerSelection === "s") ||
+        (playerSelection === "p" && computerSelection === "r") ||
+        (playerSelection === "s" && computerSelection === "p")
+    ) {
+        wins++;
+        alert("You win!");
+    }
+    else {
+        losses++;
+        alert("You lose!");
+    }
+    alert(`wins: ${wins} \nlosses: ${losses} \ndraws: ${draws}`);
+    confirm("Do you want to play again?");
+    if (confirm) {
+        startGame();
+    }
 }
 
-startGame();
-
+play();
